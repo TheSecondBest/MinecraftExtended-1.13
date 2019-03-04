c7a42d5106338c45dc4f0df7e4e6f881be98fbf1
peter1745 <peter8nilsson@live.se>
2019-03-04 11:17:26 +0100

    Added legs to the Pier and made it waterlogged

--- src/main/java/com/nmg/me/block/BlockPier.java
+++ src/main/java/com/nmg/me/block/BlockPier.java
@@ -1,55 +1,175 @@
 package com.nmg.me.block;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.nmg.me.Constants;
 import com.nmg.me.utils.VoxelShapeHelper;
 import net.minecraft.block.Block;
+import net.minecraft.block.IBucketPickupHandler;
+import net.minecraft.block.ILiquidContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.init.Fluids;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReaderBase;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BlockPier extends MEBlockFacing
+public class BlockPier extends MEBlockFacing implements IBucketPickupHandler, ILiquidContainer
 {
 
-	private final VoxelShape SHAPE;
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+	public static final EnumProperty<PierPart> PART = EnumProperty.create("part", PierPart.class);
+
+	public final ImmutableMap<IBlockState, VoxelShape> SHAPES;
 
 	public BlockPier()
 	{
 		super(Properties.create(Material.WOOD).hardnessAndResistance(2.0f));
-		SHAPE = this.generateShape();
+		this.setDefaultState(this.getStateContainer().getBaseState().with(FACING, EnumFacing.NORTH).with(PART, PierPart.TOP).with(WATERLOGGED, false));
+		SHAPES = this.generateShapes(this.getStateContainer().getValidStates());
 	}
 
-	private VoxelShape generateShape()
+	private ImmutableMap<IBlockState, VoxelShape> generateShapes(ImmutableList<IBlockState> states)
 	{
-		List<VoxelShape> shapes = new ArrayList<>();
-		shapes.add(Block.makeCuboidShape(0, 0, 0, 4, 1, 16)); // PIER_SUPPORT_RIGHT
-		shapes.add(Block.makeCuboidShape(12, 0, 0, 16, 1, 16)); // PIER_SUPPORT_LEFT
-		shapes.add(Block.makeCuboidShape(0, 1, 0, 16, 2, 3)); // PIER_PLANK_0
-		shapes.add(Block.makeCuboidShape(0, 1, 4.5, 16, 2, 7.5)); // PIER_PLANK_1
-		shapes.add(Block.makeCuboidShape(0, 1, 13, 16, 2, 16)); // PIER_PLANK_3
-		shapes.add(Block.makeCuboidShape(0, 1, 8.5, 16, 2, 11.5)); // PIER_PLANK_2
+		final VoxelShape[] BASE_0 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(0, 0, 0, 4, 1, 16));
+		final VoxelShape[] BASE_1 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(12, 0, 0, 16, 1, 16));
+		final VoxelShape[] BASE_2 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(0, 1, 0, 16, 2, 3));
+		final VoxelShape[] BASE_3 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(0, 1, 4.5, 16, 2, 7.5));
+		final VoxelShape[] BASE_4 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(0, 1, 13, 16, 2, 16));
+		final VoxelShape[] BASE_5 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(0, 1, 8.5, 16, 2, 11.5));
+		final VoxelShape[] LEG_0 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(1, 0, 1, 3, 14, 3));
+		final VoxelShape[] LEG_1 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(13, 0, 1, 15, 14, 3));
+		final VoxelShape[] LEG_2 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(13, 0, 13, 15, 14, 15));
+		final VoxelShape[] LEG_3 = VoxelShapeHelper.getRotatedVoxelShapes(Block.makeCuboidShape(1, 0, 13, 3, 14, 15));
 
-		return VoxelShapeHelper.combineAll(shapes);
+		ImmutableMap.Builder<IBlockState, VoxelShape> builder = new ImmutableMap.Builder<>();
+
+		for (IBlockState state : states)
+		{
+			List<VoxelShape> shapes = new ArrayList<>();
+			EnumFacing facing = state.get(FACING);
+			PierPart part = state.get(PART);
+
+			if (part == PierPart.TOP)
+			{
+				shapes.add(BASE_0[facing.getHorizontalIndex()]);
+				shapes.add(BASE_1[facing.getHorizontalIndex()]);
+				shapes.add(BASE_2[facing.getHorizontalIndex()]);
+				shapes.add(BASE_3[facing.getHorizontalIndex()]);
+				shapes.add(BASE_4[facing.getHorizontalIndex()]);
+				shapes.add(BASE_5[facing.getHorizontalIndex()]);
+			}
+			else if (part == PierPart.BOTTOM)
+			{
+				shapes.add(LEG_0[facing.getHorizontalIndex()]);
+				shapes.add(LEG_1[facing.getHorizontalIndex()]);
+				shapes.add(LEG_2[facing.getHorizontalIndex()]);
+				shapes.add(LEG_3[facing.getHorizontalIndex()]);
+			}
+
+			builder.put(state, VoxelShapeHelper.combineAll(shapes));
+		}
+
+		return builder.build();
 	}
 
 	@Override
 	public VoxelShape getShape(IBlockState state, IBlockReader reader, BlockPos pos)
 	{
-		return SHAPE;
+		return SHAPES.get(state);
 	}
 
 	@Override
 	public VoxelShape getCollisionShape(IBlockState state, IBlockReader reader, BlockPos pos)
 	{
-		return SHAPE;
+		return SHAPES.get(state);
 	}
 
+	@Override
+	public IBlockState getStateForPlacement(BlockItemUseContext context)
+	{
+		IBlockState state = super.getStateForPlacement(context);
+		IBlockReader world = context.getWorld();
+		BlockPos pos = context.getPos();
+		IFluidState fluidState = world.getFluidState(pos);
+
+		return state.with(PART, world.getBlockState(pos.up()).getBlock() == this ? PierPart.BOTTOM : PierPart.TOP).with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+	}
+
+	@Override
+	public IBlockState updatePostPlacement(IBlockState stateIn, EnumFacing facing, IBlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos)
+	{
+		if (stateIn.get(WATERLOGGED))
+		{
+			worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+		}
+
+		return facing.getAxis().getPlane() == EnumFacing.Plane.VERTICAL ? stateIn.with(PART, worldIn.getBlockState(currentPos.up()).getBlock() == this ? PierPart.BOTTOM : PierPart.TOP) : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+	}
+
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, IBlockState> builder)
+	{
+		super.fillStateContainer(builder);
+		builder.add(PART);
+		builder.add(WATERLOGGED);
+	}
+
+	@Override
+	public IFluidState getFluidState(IBlockState state)
+	{
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+	}
+
+	@Override
+	public Fluid pickupFluid(IWorld worldIn, BlockPos pos, IBlockState state)
+	{
+		if (state.get(WATERLOGGED))
+		{
+			worldIn.setBlockState(pos, state.with(WATERLOGGED, false), Constants.BlockStateFlags.NOTIFY_AND_UPDATE);
+			return Fluids.WATER;
+		}
+
+		return Fluids.EMPTY;
+	}
+
+	@Override
+	public boolean canContainFluid(IBlockReader worldIn, BlockPos pos, IBlockState state, Fluid fluidIn)
+	{
+		return !state.get(WATERLOGGED) && fluidIn == Fluids.WATER;
+	}
+
+	@Override
+	public boolean receiveFluid(IWorld worldIn, BlockPos pos, IBlockState state, IFluidState fluidStateIn)
+	{
+		if (!state.get(WATERLOGGED) && fluidStateIn.getFluid() == Fluids.WATER)
+		{
+			if (!worldIn.isRemote())
+			{
+				worldIn.setBlockState(pos, state.with(WATERLOGGED, true), Constants.BlockStateFlags.NOTIFY_AND_UPDATE);
+				worldIn.getPendingFluidTicks().scheduleTick(pos, fluidStateIn.getFluid(), fluidStateIn.getFluid().getTickRate(worldIn));
+			}
+
+			return true;
+		}
+
+		return false;
+	}
 
 	@Override
 	public boolean canPlaceTorchOnTop(IBlockState state, IWorldReaderBase world, BlockPos pos)
@@ -67,6 +187,26 @@ public class BlockPier extends MEBlockFacing
 	public BlockFaceShape getBlockFaceShape(IBlockReader worldIn, IBlockState state, BlockPos pos, EnumFacing face)
 	{
 		return BlockFaceShape.UNDEFINED;
+	}
+
+	public enum PierPart implements IStringSerializable
+	{
+		TOP("top"),
+		BOTTOM("bottom")
+		;
+
+		private final String name;
+
+		PierPart(String name)
+		{
+			this.name = name;
+		}
+
+		@Override
+		public String getName()
+		{
+			return this.name;
+		}
 	}
 
 }
